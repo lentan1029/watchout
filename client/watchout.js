441213a7ffc1cff19523cd8188f642084792ab3a
lentan <lentan1029@gmail.com>
2016-08-05 17:51:09 -0700

    Draw the enemies in an SVG element
    added asteroids.png

--- client/watchout.js
+++ client/watchout.js
@@ -3,19 +3,49 @@
 //   hihi('nobody');
 // });
 
-var dataset = [0, 10, 2, 3, 14, 5];
+var randomAsteroidGenerator = function(index) {
+  return {
+    id: index,
+    x: Math.random() * 95,
+    y: Math.random() * 95
+  };
+};
+
+var dataset = [];
+
+for (var i = 0; i < 20; i++) {
+  dataset.push(randomAsteroidGenerator(i));
+}
+
+console.log(dataset);
+
 var svg = d3.select('body').append('svg').attr('position', 'fixed')
 .attr('top', 0)
 .attr('bottom', 0)
 .attr('width', '100%')
 .attr('height', '100%'); 
 
-svg.selectAll('circle').data(dataset).enter().append('circle')
-.attr('cx', function(d, i) {
-  return ((100 / dataset.length) * i).toString() + '%';
+svg.selectAll('image').data(dataset).enter().append('image')
+.attr('xlink:href', './asteroid.png')
+.attr('x', function(d) {
+  return d.x.toString() + '%';
 })
-.attr('cy', '50%')
-.attr('r', function(d) {
-  return d;
-});
+.attr('y', function(d) {
+  return d.y.toString() + '%';
+})
+.attr('height', '5%')
+.attr('width', '5%');
+// .attr('cx', function(d, i) {
+//   return ((100 / dataset.length) * i).toString() + '%';
+// })
+// .attr('cy', '50%')
 
+// .attr('r', function(d) {
+//   return d;
+// });
+
+var randomMove = function() {
+
+};
+
+// window.setInterval()
