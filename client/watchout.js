33839278007a235d8875afc55fb64e1eabebda70
lentan <lentan1029@gmail.com>
2016-08-05 20:21:27 -0700

    Everythings working

--- client/watchout.js
+++ client/watchout.js
@@ -8,9 +8,7 @@ var randomAsteroidGenerator = function(index) {
     id: index,
     x: Math.random() * 95,
     y: Math.random() * 95,
-    radius: 0.035,
-    vx: 1,
-    vy: 1
+    radius: 0.035
   };
 };
 
@@ -37,38 +35,22 @@ svg.selectAll('image').data(dataset).enter().append('image')
 .attr('height', '5%')
 .attr('width', '5%');
 
-// var force = d3.forceSimulation(dataset);
+// window.setInterval(collide, 50);
 
-// force.on('tick', function() {
-//   console.log('hihihi');
-//   d3.selectAll('image').data(dataset, function(item) {
-//     return item.id;
-//   }).transition().duration(1000)
-//   .attr('x', function(d) {
-//     return d.x.toString() + '%';
-//   })
-//   .attr('y', function(d) {
-//     return d.y.toString() + '%';
-//   });
-//   force.ticks = 10;
-// });
+// var collide = function() {
 
-window.setInterval(collide, 50);
+//   var checkCollision = function (current, other) {
+//     var distanceSquared = Math.pow((current.x - other.x), 2) + Math.pow((current.y - other.y), 2);
+//     if (distanceSquared < Math.pow(current.radius + other.radius, 2)) {
+//       return true;
+//     }
 
-var collide = function() {
 
-  var checkCollision = function (current, other) {
-    var r = current.radius * 100; //maybe add some padding. current.radius returns decimal
-    var cleft = current.x - r;
-    var cright = current.x + r;
-    var ctop = current.y - r;
-    var cbtm = current.y + r;
-    var oleft = other.x - r;
-    var oright = other.x + r;
-    var otop = other.y - r;
-    var obtm = other.y + r;
-  };
-};
+
+//   };
+
+
+// };
 
 var randomMove = function() {
   for (var i = 0; i < dataset.length; i++) {
@@ -76,6 +58,54 @@ var randomMove = function() {
     dataset[i].y = Math.random() * 95;
   }
 
+  d3.selectAll('image').data(dataset, function(item) {
+    return item.id;
+  }).transition().duration(1000)
+  .attr('x', function(d) {
+    return d.x.toString() + '%';
+  })
+  .attr('y', function(d) {
+    return d.y.toString() + '%';
+  });
+
 };
 
 window.setInterval(randomMove, 2000);
+
+
+var circles = d3.range(1).map(function() {
+  return {
+    // x: Math.round(Math.random() * (width - radius * 2) + radius),
+    // y: Math.round(Math.random() * (height - radius * 2) + radius)
+    x: (Math.random() * 95) + '%',
+    y: (Math.random() * 95) + '%'
+  };
+});
+
+var color = d3.scaleOrdinal()
+    .range(d3.schemeCategory20);
+
+
+var dragstarted = function(d) {
+  d3.select(this).raise().classed('active', true);
+};
+
+var dragged = function(d) {
+  d3.select(this).attr('cx', d.x = d3.event.x).attr('cy', d.y = d3.event.y);
+};
+
+var dragended = function(d) {
+  d3.select(this).classed('active', false);
+};
+
+d3.select('svg').selectAll('circle')
+  .data(circles)
+  .enter().append('circle')
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', 8)
+    .style('fill', function(d, i) { return color(i); })
+    .call(d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended));
